465312f6d432c95f66dde951599e162bf9ed06d8
Dane Sisinni <dfsisinni@gmail.com>
2016-02-10 14:37:02 -0500

    initial commit w SMS servlet

---
+++ src/com/design/servlets/SMSServlet.java
@@ -0,0 +1,31 @@
+package com.design.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SMSServlet
+ */
+@WebServlet("/sms")
+public class SMSServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SMSServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	if (request.getParameter("From") != null) {
+    		System.out.println(request.getParameter("From"));
+            System.out.println(request.getParameter("Body"));
+    	}
+    }
+}
