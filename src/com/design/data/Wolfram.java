7659c488203f2375820f36871cadcd5f68e2828b
ShaynAli <shayaan.syed.ali@gmail.com>
2016-02-27 00:34:52 -0500

    Basic (re-)implementation of Wolfram Module

--- src/com/design/data/Wolfram.java
+++ src/com/design/data/Wolfram.java
@@ -1,31 +1,36 @@
 /**
  * The Wolfram Module, deals with queries relevant to Wolfram Alpha
+ * Specifically, this module handles:
+ * 
+ * @author Shayaan Syed Ali
+ * @version Feb 26, 2016
  */
 package com.design.data;
 
 import com.design.communicate.Communicate;
 import com.wolfram.alpha.WAEngine;
 import com.wolfram.alpha.WAException;
-import com.wolfram.alpha.WAPlainText;
-import com.wolfram.alpha.WAPod;
 import com.wolfram.alpha.WAQuery;
 import com.wolfram.alpha.WAQueryResult;
-import com.wolfram.alpha.WASubpod;
 
-public interface Wolfram {
+// This was originally an interface, it may be better as a class or it may not be
+public class Wolfram
+{
 
-	public static void wolframAlpha(String body)
+	public static void wolframAlpha(String queryStr)
 	{
-    	String result = ".\n";
+    	StringBuilder result = new StringBuilder(".\n");
     	
+    	// Basic engine setup
     	WAEngine engine = new WAEngine();
     	engine.setAppID("7AHUTR-UV58KYXA8Q");
     	 
+    	// Query set up using parameter
     	WAQuery query = engine.createQuery();
-    	query.setInput(body);
-    	 
-    	WAQueryResult queryResult = null;
+    	query.setInput(queryStr);
     	
+    	// Query retrieval and error handling
+    	WAQueryResult queryResult = null;
 		try
 		{
 			queryResult = engine.performQuery(query);
@@ -33,58 +38,82 @@ public interface Wolfram {
 		catch (WAException e)
 		{
 			// WIP: Handle WA engine errors
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	 
-		if (queryResult.isError())
-		{
-    		 // WIP: Handles error from Wolfram querying
+			e.printStackTrace(); // Might not want this; need to generate a response
 		}
 		
-		else {
-			outerloop:
+		// Error case
+		if (queryResult.isError())
+		{
+			System.out.println("Query error: " + queryResult.getErrorCode() + ": " + queryResult.getErrorMessage());
+			// WIP: Handles error from Wolfram querying
+		}
+		else if (queryResult.isSuccess())
+		{
+			System.out.println();
+			// WIP: Handles failure of Wolfram to find
+		}
+		// Response generation
+		else
+		{
+			// To do:
+			// 1. Determine what the query is relevant to
+			// 2. Organize the query as needed (if needed)
+			// 3. Set up assumptions (as needed)
+			// 4. Get WA engine response
+			// 5. Sort through response
+			// 6. Generate result with parts of response
 
-				for (WAPod pod : queryResult.getPods()) {
+			
+		}
 
-					if (!pod.isError()) {
+			
 
-    				 for (WASubpod subpod : pod.getSubpods()) {
-    					 for (Object element : subpod.getContents()) {
-    						 if (element instanceof WAPlainText) {
-    							  
-    							 if (body.toLowerCase().contains("derivative") || body.toLowerCase().contains("deriv")) {
-    								 if (((WAPlainText) element).getText().contains("d/dx")) {
-    									 result +=  ((WAPlainText) element).getText();
-    									 if (body.toLowerCase().contains("integral") || body.toLowerCase().contains("integrate") ||
-    											 body.toLowerCase().contains("derivative")) {
-    										 break outerloop;
-    									 }
-    									 
-    								 }
-    							 } else {
-    								 if (!((WAPlainText) element).getText().contains("Plot")) {
-        								 System.out.println(((WAPlainText) element).getText());  
-        								 result += ((WAPlainText) element).getText();
-        								 if (body.toLowerCase().contains("integral") || body.toLowerCase().contains("integrate") ||
-    											 body.toLowerCase().contains("derivative")) {
-    										 break outerloop;
-    									 }
-        							 }
-    							 }
-    						 }
-    					 }
-    				 }
-    			 }
-    		 }
-    	 }
+
+    	 // Send results through Communicate Module
+    	 Communicate.sendText(result.toString());
     	 
-    	 result = result.replace("+", " + ");
-    	 result = result.replace("-", " - ");
-    	 result = result.replace("constant", "C");
-    	 result = result.replace("=", "\n=");
+	} // wolframAlpha method
+		
+} // Wolfram class
     	 
-    	 Communicate.sendText(result);
-    }
-	
-}
+    	// Dane's old code (in 'Response generation' else block
+//			outerloop:
+//
+//				for (WAPod pod : queryResult.getPods()) {
+//
+//					if (!pod.isError()) {
+//
+// 				 for (WASubpod subpod : pod.getSubpods()) {
+// 					 for (Object element : subpod.getContents()) {
+// 						 if (element instanceof WAPlainText) {
+// 							  
+// 							 if (body.toLowerCase().contains("derivative") || body.toLowerCase().contains("deriv")) {
+// 								 if (((WAPlainText) element).getText().contains("d/dx")) {
+// 									 result +=  ((WAPlainText) element).getText();
+// 									 if (body.toLowerCase().contains("integral") || body.toLowerCase().contains("integrate") ||
+// 											 body.toLowerCase().contains("derivative")) {
+// 										 break outerloop;
+// 									 }
+// 									 
+// 								 }
+// 							 } else {
+// 								 if (!((WAPlainText) element).getText().contains("Plot")) {
+//     								 System.out.println(((WAPlainText) element).getText());  
+//     								 result += ((WAPlainText) element).getText();
+//     								 if (body.toLowerCase().contains("integral") || body.toLowerCase().contains("integrate") ||
+// 											 body.toLowerCase().contains("derivative")) {
+// 										 break outerloop;
+// 									 }
+//     							 }
+// 							 }
+// 						 }
+// 					 }
+// 				 }
+// 			 }
+// 		 }
+ 	 
+//Dane's old code (right before 'Communicate.sendtext(result);' line
+// 	 result = result.replace("+", " + ");
+// 	 result = result.replace("-", " - ");
+// 	 result = result.replace("constant", "C");
+// 	 result = result.replace("=", "\n=");
